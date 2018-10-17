a3f7e7a30265cb24f65092089d68b23e33be39a7
Brandon Delehoy <bdelehoy@yahoo.com>
2018-10-17 16:59:03 -0700

    Implement initial player animation; minor cleanup

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -11,26 +11,20 @@ func _process(delta):
 	var velocity = Vector2()
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-		#move_and_slide(velocity.normalized()*speed,Vector2(0,0))
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-		#move_and_slide(velocity.normalized()*speed,Vector2(0,0))
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-		#move_and_slide(velocity.normalized()*speed,Vector2(0,0))
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
-		#move_and_slide(velocity.normalized()*speed,Vector2(0,0))
+
 	move_and_slide(velocity.normalized()*speed,Vector2(0,0))
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
-	
-	#position += velocity * delta
-	#position.x = clamp(position.x, 0, screensize.x)
-	#position.y = clamp(position.y, 0, screensize.y)
 	
 	if velocity.x > 0:
 		$AnimatedSprite.animation = "right"
