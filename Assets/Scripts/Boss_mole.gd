b99cee5f2275b9628b5e875afb52621388b8c86c
Mayan Shoshani <mshoshan@uci.edu>
2018-11-20 14:38:02 -0800

    mole boss shoots 3 turd

--- Assets/Scripts/Boss_mole.gd
+++ Assets/Scripts/Boss_mole.gd
@@ -3,6 +3,9 @@ extends "res://Assets/Scripts/mole.gd"
 var nwp2
 var np2
 
+var nwp3
+var np3
+
 func _ready():
 	setup(20, 74, 150, 300)
 	
@@ -30,6 +33,16 @@ func throw():
 	nwp2.set_damage(1)
 	nwp2.add_collision_exception_with(self)
 	get_tree().get_root().add_child(nwp2) 	
+	
+	nwp3 = projectilePre.instance()
+	p = position
+	p.x = p.x-30
+	np3 = position
+	np3.x=np3.x-10
+	nwp3.set_v(Vector2(playerDist-p),np3)
+	nwp3.set_damage(1)
+	nwp3.add_collision_exception_with(self)
+	get_tree().get_root().add_child(nwp3) 	
 	
 	timer = 74
 	isThrowing = false;
