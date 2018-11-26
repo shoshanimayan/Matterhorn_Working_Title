df8f7eef91553cefdcab57694d67914ae42c8263
John Luke Saake <johnlukesaake@gmail.com>
2018-11-25 22:46:27 -0800

    Website: Front Page Opacity
    Front page images now change opacity on mouse hover.

--- Website/js/index.js
+++ Website/js/index.js
@@ -1,3 +1,11 @@
 function clickLink(location) {
     window.open(location, "_self");
 }
+
+function hoverOn(id) {
+    document.getElementById(id).style.opacity = 0.2;
+}
+
+function hoverOff(id) {
+    document.getElementById(id).style.opacity = .7;
+}
