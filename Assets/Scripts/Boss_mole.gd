b383fc5524167150c2cf100b6ab5d3b31779c93e
Brandon Delehoy <bdelehoy@yahoo.com>
2018-11-28 15:53:11 -0800

    Remove 2nd boss projectile (performance issues?)

--- Assets/Scripts/Boss_mole.gd
+++ Assets/Scripts/Boss_mole.gd
@@ -21,19 +21,20 @@ func throw():
 	newProjectile.set_v(Vector2(playerDist-position),newPosition)
 	newProjectile.set_damage(1)
 	newProjectile.add_collision_exception_with(self)
-	get_tree().get_root().add_child(newProjectile) 	
+	get_tree().get_root().add_child(newProjectile)
 	
-	nwp2 = projectilePre.instance()
-	
-	p = position
-	p.x = p.x+30
-	np2 = position
-	np2.x=np2.x+10
-	nwp2.set_v(Vector2(playerDist-p),np2)
-	nwp2.set_damage(1)
-	nwp2.add_collision_exception_with(self)
-	get_tree().get_root().add_child(nwp2) 	
-	
+	# Uncomment below to engage toggle projectile (may affect performance)
+#	nwp2 = projectilePre.instance()
+#
+#	p = position
+#	p.x = p.x+30
+#	np2 = position
+#	np2.x=np2.x+10
+#	nwp2.set_v(Vector2(playerDist-p),np2)
+#	nwp2.set_damage(1)
+#	nwp2.add_collision_exception_with(self)
+#	get_tree().get_root().add_child(nwp2)
+
 	"""nwp3 = projectilePre.instance()
 	p = position
 	p.x = p.x-30
