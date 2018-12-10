63cd45f7c751c6483763f1ba99e752fba56d2fc5
Mayan Shoshani <mshoshan@uci.edu>
2018-12-10 01:04:46 -0800

    trash stuff

--- Assets/Scripts/trashcan.gd
+++ Assets/Scripts/trashcan.gd
@@ -6,38 +6,40 @@ var newItem3
 var newItem4
 var newItem5
 
-
+var x =0
 
 
 
 func hit(x):
-	self.hide()
-	newItem = trashSmallDrop.instance()
-	newItem.position = position
-	
-	var newItem2 = trashSmallDrop.instance()
-	var newItem3 =  trashSmallDrop.instance()
-	var newItem4 =  trashSmallDrop.instance()
-	var newItem5 =  trashSmallDrop.instance()
-	
-	get_tree().get_root().add_child(newItem)
-	
-	newItem2.position = position
-	newItem2.position.x = position.x-30
-	get_tree().get_root().add_child(newItem2)
-	
-	newItem3.position = position
-	newItem3.position.x = position.x+30
-	get_tree().get_root().add_child(newItem3)
-	
-	newItem4.position = position
-	newItem4.position.y = position.y-30
-	get_tree().get_root().add_child(newItem4)
-	
-	newItem5.position = position
-	newItem5.position.y = position.y+30
-	get_tree().get_root().add_child(newItem5)
-	
-	#kill mole process
-	self.queue_free()
+	if x==0:
+		x=15
+		self.hide()
+		newItem = trashSmallDrop.instance()
+		newItem.position = position
+		
+		var newItem2 = trashSmallDrop.instance()
+		var newItem3 =  trashSmallDrop.instance()
+		var newItem4 =  trashSmallDrop.instance()
+		var newItem5 =  trashSmallDrop.instance()
+		
+		get_tree().get_root().add_child(newItem)
+		
+		newItem2.position = position
+		newItem2.position.x = position.x-30
+		get_tree().get_root().add_child(newItem2)
+		
+		newItem3.position = position
+		newItem3.position.x = position.x+30
+		get_tree().get_root().add_child(newItem3)
+		
+		newItem4.position = position
+		newItem4.position.y = position.y-30
+		get_tree().get_root().add_child(newItem4)
+		
+		newItem5.position = position
+		newItem5.position.y = position.y+30
+		get_tree().get_root().add_child(newItem5)
+		
+		#kill mole process
+		self.queue_free()
 
